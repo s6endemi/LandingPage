f475fcaa86d9667e49e2cfe1161c5271f9796ea1
fischern0 <s6nsfisc@uni-bonn.de>
2024-10-25 20:16:15 +0200

    add lucide icon

--- src/lib/components/WorkoutPlannerForms/ExperienceLevel.svelte
+++ src/lib/components/WorkoutPlannerForms/ExperienceLevel.svelte
@@ -3,6 +3,7 @@
   import beginnerImg from "$lib/assets/beginner.png";
   import intermediateImg from "$lib/assets/intermediate.png";
   import expertImg from "$lib/assets/expert.png";
+  import { Info } from "lucide-svelte";
 
   interface CardButtonProps {
     level: Level;
@@ -71,25 +72,10 @@
     <label class="text-2xl font-bold text-neutral-content" for="experience-level">
       Wie erfahren bist du?
       <div
-        class="tooltip tooltip-right"
+        class="tooltip tooltip-right top-1 align-top"
         data-tip="Wähle deine Erfahrungsstufe: Anfänger, Fortgeschritten oder Experte."
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-info"
-        >
-          <circle cx="12" cy="12" r="10" />
-          <path d="M12 16v-4" />
-          <path d="M12 8h.01" />
-        </svg>
+        <Info />
       </div>
     </label>
 
