a2bb073cbb014a0068991ddfd79362ce8dca6a66
Nils Fischer <nils-fischer7@web.de>
2025-02-08 11:43:31 +0100

    remove unnecessary routes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,9 @@
-import { getRedisValue, setRedisValue } from "$lib/server/redis";
 import { supabase } from "$lib/server/supabaseClient";
 import { type Gender, type Profile } from "$lib/types";
 import type { LayoutServerLoad } from "./$types";
 
 async function getProfileById(id: string | undefined): Promise<Profile | null> {
   if (id === undefined) return null;
-  const cachedProfile = await getRedisValue<Profile>(`profile:${id}`);
-  if (cachedProfile) return cachedProfile;
 
   const { data, error } = await supabase.from("profiles").select("*").eq("id", id).single();
 
@@ -23,7 +20,6 @@ async function getProfileById(id: string | undefined): Promise<Profile | null> {
     gender: data.gender as Gender,
   };
   // cache for 5 minutes
-  setRedisValue(`profile:${id}`, profile, 300);
   return profile;
 }
 
