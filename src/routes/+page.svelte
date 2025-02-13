7a82c36b919256ea82e3bf6d97f742dac95db948
Eren <erendemir10022@gmail.com>
2025-02-13 17:36:17 +0100

    added cyrpto address

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -285,7 +285,7 @@
   // Clipboard function
   const copyToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText("0x742d35Cc6634C0532925a3b844Bc454e4438f44e");
+      await navigator.clipboard.writeText("");
       isCopied = true;
       setTimeout(() => {
         isCopied = false;
