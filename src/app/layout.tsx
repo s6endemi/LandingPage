148579234c9568863e80aaff6a186832c715da35
Eren <erendemir10022@gmail.com>
2025-09-20 14:07:13 +0200

    anpasungen2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             strokeWidth={0.5}
             width={40}
             height={40}
-            gridSize={30}
+            numSquares={30}
             patternColor="rgba(120, 120, 130, 0.4)"
           />
         </div>
